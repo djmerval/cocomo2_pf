531d9c7b29ea26dcaaf04e6383edf08b050157a5
AnthonyDOM <anthonyortega164@gmail.com>
2020-07-24 12:12:18 -0500

    Eliminacion de Comentarios

--- cocomo2_pf/src/cocomo2_pf/Cocomo2_pf.java
+++ cocomo2_pf/src/cocomo2_pf/Cocomo2_pf.java
@@ -1,21 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cocomo2_pf;
 
-/**
- *
- * @author diego
- */
 public class Cocomo2_pf {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("hola que hace");
-    }
-    
+    } 
 }
